13413c785fccd08d1d3124aa71422fbedcc1f756
Jeremy Rogers <93287920+JollyRen@users.noreply.github.com>
2023-12-03 22:23:22 -0700

    update test.js

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 
-for (var i = 0; i < 10; i++) {
+for (let i = 0; i < 10; i++) {
   // if (!(i%2)) console.log('odd')
   // if (i%2) console.log('even')
   i%2 ? console.log('even') : console.log('odd')
